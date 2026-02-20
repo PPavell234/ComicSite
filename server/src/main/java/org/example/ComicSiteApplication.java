4bbb707558c7022e279a8805c34b0484f7f7eb19
PPavell234 <varvardidl@gmail.com>
2026-02-20 20:19:20 +1000

    Initial commit

---
+++ server/src/main/java/org/example/ComicSiteApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComicSiteApplication.class, args);
+    }
+}
