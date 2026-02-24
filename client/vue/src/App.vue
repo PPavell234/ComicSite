613c7f76da45a79fd702b5e80261d7d46c6b1171
PPavell234 <varvardidl@gmail.com>
2026-02-24 18:18:16 +1000

    Подключение tailwild

--- client/vue/src/App.vue
+++ client/vue/src/App.vue
@@ -3,9 +3,18 @@
 <template>
   <h1>You did it!</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    Visit <a class="text-blue-600/100 dark:text-sky-400/100" href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+
+  <div class="grid grid-cols-3 gap-2 place-items-start h-48 ...">
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+  </div>
 </template>
 
 <style scoped></style>
