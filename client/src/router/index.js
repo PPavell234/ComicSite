738e6c50fc7f2ff62377a25b974c384606556fe6
PPavell234 <varvardidl@gmail.com>
2026-03-10 23:54:05 +1000

    Добалвение компонента
    Нэдера

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,16 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import headerMenu from "../views/headerMenu.vue";
+import headerMenu from "../views/headerMenuf.vue";
 import HelloWorld from "../components/HelloWorld.vue";
 import readComic from "../views/readComic.vue";
 import comicP from "../components/Page/comicP.vue";
 import UserList from "../components/UserList.vue";
+import MainPage from "../views/mainPage.vue";
+import HeaderMenuf from "../views/headerMenuf.vue";
+import HeaderMenu from "../components/Page/headerMenu.vue";
 
 const routes = [
+  //Комонент
+  {
+    path: "/headerMenu",
+    name: "headerMenu",
+    component: HeaderMenu,
+  },
   {
     path: "/",
     name: "ComicP",
     component: comicP,
+  },
+  {
+    path: "/ru",
+    name: "MainPage",
+    component: MainPage,
   },
   {
     path: "/readComic",
@@ -23,9 +37,9 @@ const routes = [
     component: UserList,
   },
   {
-    path: "/headerMenu",
-    name: "headerMenu",
-    component: headerMenu,
+    path: "/headerMenuf",
+    name: "headerMenuf",
+    component: HeaderMenuf,
   },
   {
     path: "/helloWorld",
